08f6b4bc8d2834a2a06a43493c06c2639b71b716
renancvitor <renan.vitor.cm@gmail.com>
2024-12-08 10:02:40 -0300

    Added views Cadastrar Cargo/Departamento

--- src/telas/MainViewController.java
+++ src/telas/MainViewController.java
@@ -42,7 +42,7 @@ public class MainViewController implements Initializable {
 	
 	@FXML
 	public void onMenuItemDepartamento() {
-		System.out.println("onMenuItemDepartamento");
+		loadView("/telas/CadastroDepartamento.fxml");
 	}
 	
 	@FXML
