7c77284330a6729d08536d1735ac9d3d15a9c02b
renancvitor <renan.vitor.cm@gmail.com>
2024-12-16 23:02:09 -0300

    Checking database connection

--- src/db/DB.java
+++ src/db/DB.java
@@ -10,16 +10,17 @@ import java.sql.Statement;
 import java.util.Properties;
 
 public class DB {
-	
+
 	private static Connection conn = null;
 	
 	public static Connection getConnection() {
 		if (conn == null) {
 			try {
 				Properties props = loadProperties();
-				String url = props.getProperty("db.url");
+				String url = props.getProperty("dburl");
 				conn = DriverManager.getConnection(url, props);
-			} catch (SQLException e) {
+			}
+			catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
@@ -33,24 +34,24 @@ public class DB {
 			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
-			
 		}
 	}
-
+	
 	private static Properties loadProperties() {
-		try (FileInputStream fs = new FileInputStream("config.properties")) {
+		try (FileInputStream fs = new FileInputStream("db.properties")) {
 			Properties props = new Properties();
 			props.load(fs);
 			return props;
-		} catch (IOException e) {
+		}
+		catch (IOException e) {
 			throw new DbException(e.getMessage());
 		}
 	}
 	
-	public static void closeStatement(Statement stmt) {
-		if (stmt != null) {
+	public static void closeStatement(Statement st) {
+		if (st != null) {
 			try {
-				stmt.close();
+				st.close();
 			} catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,9 +1,16 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.AlterarSenhaDaoJDBC;
 import model.dao.impl.CargoDaoJDBC;
 import model.dao.impl.DepartamentoDaoJDBC;
+import model.dao.impl.LoginDaoJDBC;
+import model.dao.impl.NovaSolicitacaoDaoJDBC;
 import model.dao.impl.PessoaDaoJDBC;
+import model.dao.impl.SolicitacoesAnalisadasDaoJDBC;
+import model.dao.impl.SolicitacoesEnviadasDaoJDBC;
+import model.dao.impl.UserPermissionsDaoJDBC;
+import model.dao.impl.UserTypeDaoJDBC;
 import model.dao.impl.UsuarioDaoJDBC;
 
 public class DaoFactory {
@@ -22,5 +29,33 @@ public class DaoFactory {
 	
 	public static PessoaDao createPessoaDao() {
 		return new PessoaDaoJDBC(DB.getConnection());
+	}
+	
+	public static AlterarSenhaDao createAlterarSenhaDao() {
+		return new AlterarSenhaDaoJDBC(DB.getConnection());
+	}
+	
+	public static LoginDao createLoginDao() {
+		return new LoginDaoJDBC(DB.getConnection());
+	}
+	
+	public static NovaSolicitacaoDao createNovaSolicitacaoDao() {
+		return new NovaSolicitacaoDaoJDBC(DB.getConnection());
+	}
+	
+	public static SolicitacoesAnalisadasDao createSolicitacoesAnalisadasDao() {
+		return new SolicitacoesAnalisadasDaoJDBC(DB.getConnection());
+	}
+	
+	public static SolicitacoesEnviadasDao createSolicitacoesEnviadasDao() {
+		return new SolicitacoesEnviadasDaoJDBC(DB.getConnection());
+	}
+	
+	public static UserTypeDao createUserTypeDao() {
+		return new UserTypeDaoJDBC(DB.getConnection());
+	}
+	
+	public static UserPermissionsDao createUserPermissiosnDao() {
+		return new UserPermissionsDaoJDBC(DB.getConnection());
 	}
 }
