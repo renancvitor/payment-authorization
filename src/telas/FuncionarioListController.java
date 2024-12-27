0498ed2198841187affb48df8ca22a98146b35ef
renancvitor <renan.vitor.cm@gmail.com>
2024-12-27 09:39:48 -0300

    Creation of FuncionarioForm and UsuarioForm FXML

--- src/telas/FuncionarioListController.java
+++ src/telas/FuncionarioListController.java
@@ -1,25 +1,33 @@
 package telas;
 
+import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Cargo;
 import model.entities.Departamento;
 import model.entities.Pessoa;
 import model.services.FuncionarioService;
+import telas.util.Alerts;
+import telas.util.utils;
 
 public class FuncionarioListController implements Initializable {
 	
@@ -52,8 +60,9 @@ public class FuncionarioListController implements Initializable {
 	private ObservableList<Pessoa> obsList;
 	
 	@FXML
-	public void onBtNewAction() {
-		System.out.println("onBtNewAction");
+	public void onBtNewAction(ActionEvent event) {
+		Stage parentStage = utils.currentStage(event);
+		createDialogForm("/telas/CargoForm.fxml", parentStage);
 	}
 	
 	public void setFuncionarioService(FuncionarioService service) {
@@ -88,4 +97,22 @@ public class FuncionarioListController implements Initializable {
 		tableViewFuncionario.setItems(obsList);
 	}
 
+	private void createDialogForm(String absoluteName, Stage parentStage) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			Pane pane = loader.load();
+			
+			Stage dialogStage = new Stage();
+			dialogStage.setTitle("Enter cargo data");
+			dialogStage.setScene(new Scene(pane));
+			dialogStage.setResizable(false);
+			dialogStage.initOwner(parentStage);
+			dialogStage.initModality(Modality.WINDOW_MODAL);
+			dialogStage.showAndWait();
+			
+		} catch (IOException e) {
+			Alerts.showAlert("IO Exception", "Error loading view", e.getMessage(), AlertType.ERROR);
+		}
+	}
+	
 }
