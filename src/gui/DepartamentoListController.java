6fd179eeb5fb155181cc3ac3accba9eb241de247
renancvitor <renan.vitor.cm@gmail.com>
2025-04-20 18:42:22 -0300

    Estruturação atualizada DepartamentoListController

--- src/gui/DepartamentoListController.java
+++ src/gui/DepartamentoListController.java
@@ -53,7 +53,8 @@ public class DepartamentoListController implements Initializable, DataChangeList
 	    
 	    Stage currentStage = (Stage) Main.getMainScene().getWindow();
 	    
-	    Alerts.showAlertWithOwner("Logout", "Você foi desconectado", "Você foi desconectado do sistema.", AlertType.INFORMATION, currentStage);
+	    Alerts.showAlertWithOwner("Logout", "Você foi desconectado",
+				"Você foi desconectado do sistema.", AlertType.INFORMATION, currentStage);
 
 	    currentStage.close();
 
@@ -62,7 +63,8 @@ public class DepartamentoListController implements Initializable, DataChangeList
 	        Main mainApp = new Main();
 	        mainApp.start(loginStage);
 	    } catch (Exception e) {
-	        Alerts.showAlert("Erro", "Erro ao retornar à tela de login", e.getMessage(), AlertType.ERROR);
+	        Alerts.showAlert("Erro", "Erro ao retornar à tela de login",
+					e.getMessage(), AlertType.ERROR);
 	        e.printStackTrace();
 	    }
 	}
@@ -128,5 +130,4 @@ public class DepartamentoListController implements Initializable, DataChangeList
 	public void onDataChanged() {
 		updateTableView();
 	}
-
 }
