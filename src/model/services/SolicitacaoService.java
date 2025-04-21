dc9fd129562d0caaa67c14e26c6c9a00a8e5c826
renancvitor <renan.vitor.cm@gmail.com>
2025-04-20 21:25:50 -0300

    Estruturação atualizada SolicitacaoService

--- src/model/services/SolicitacaoService.java
+++ src/model/services/SolicitacaoService.java
@@ -24,25 +24,32 @@ public class SolicitacaoService {
 
     public List<SolicitacoesAnalisadas> getSolicitacoesVisiveisParaUsuario(Usuario usuario) throws SQLException {
         if (permissaoService.visualizarTodasSolicitacoes(usuario)) {
-            return solicitacaoDAO.select(StatusSolicitacao.PENDENTE, usuario.getUserType().getId(), usuario.getId());
+            return solicitacaoDAO.select(StatusSolicitacao.PENDENTE, usuario.getUserType().getId(),
+                    usuario.getId());
         }
 
         if (usuario.getUserType() == UserType.COMUM) {
-            return solicitacaoDAO.select(StatusSolicitacao.PENDENTE, usuario.getUserType().getId(), usuario.getId());
+            return solicitacaoDAO.select(StatusSolicitacao.PENDENTE, usuario.getUserType().getId(),
+                    usuario.getId());
         }
         return solicitacaoDAO.select(StatusSolicitacao.PENDENTE, usuario.getUserType().getId(), usuario.getId());
     }
 
-    public List<SolicitacoesAnalisadas> getSolicitacoesPorStatus(StatusSolicitacao status, int idTipoUsuario, int idUser) throws SQLException {
+    public List<SolicitacoesAnalisadas> getSolicitacoesPorStatus(StatusSolicitacao status,
+                                                                 int idTipoUsuario, int idUser)
+            throws SQLException {
         return solicitacaoDAO.select(status, idTipoUsuario, idUser);
     }
 
-    public void atualizarStatusSolicitacao(Usuario usuario, PermissaoService permissaoService, SolicitacoesEnviadas solicitacao, StatusSolicitacao novoStatus) throws SQLException {
+    public void atualizarStatusSolicitacao(Usuario usuario, PermissaoService permissaoService,
+                                           SolicitacoesEnviadas solicitacao, StatusSolicitacao novoStatus)
+            throws SQLException {
         if (permissaoService.aprovarReprovarSolicitacoes(usuario)) {
             solicitacao.setStatus(novoStatus);
             solicitacoesEnviadasDao.update(solicitacao);
         } else {
-            Alert alert = new Alert(Alert.AlertType.WARNING, "Acesso negado: Usuário não tem permissão para aprovar ou reprovar solicitações.");
+            Alert alert = new Alert(Alert.AlertType.WARNING,
+                    "Acesso negado: Usuário não tem permissão para aprovar ou reprovar solicitações.");
             alert.show();
         }
     }
